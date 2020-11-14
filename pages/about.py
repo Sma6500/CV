573e9eb94bb559a52ce9e8db4f533ed9ef650045
Sma6500 <56174379+Sma6500@users.noreply.github.com>
2020-11-14 17:57:00 +0100

    Update about.py

--- pages/about.py
+++ pages/about.py
@@ -5,12 +5,12 @@ def content():
     st.write(
         "I'm a 22 french engineering student at __IMT Atlantique__, one of the top french _grandes écoles_.    \n"
         "Currently in a gap year, I am in charge of the statistical studies of Direct Assurance a french insuring company \
-        until February. \n"
-        "I'm searching for another internship for the second half of my gap year."
+        until February.    \n"
+        "I'm searching for another internship for the second half of my gap year.   \n"
         "During my studies, I specialised in __machine-learning__, __Data analysis__ and its  \
-        applications in business, especially digital business.\n"
+        applications in business, especially digital business.   \n"
         "I also studied and implement __deep-learning__ algorithms and I'm very interested in  \
-        improving and putting in practice my skills in this field.\n"
+        improving and putting in practice my skills in this field.   \n"
         "My main skills include python development in general, more specifically Sklearn and Pytorch. ")
         
     st.write("You can find my github [here](https://github.com/Sma6500)")
