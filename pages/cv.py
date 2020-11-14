a678fa4d635cf3a89ef08be35d061523c87f9725
Sma6500 <56174379+Sma6500@users.noreply.github.com>
2020-11-14 17:56:08 +0100

    Update cv.py

--- pages/cv.py
+++ pages/cv.py
@@ -5,18 +5,20 @@ def experiences():
     experiences = st.beta_expander("Experiences")
     experiences.markdown(
         " __2020__: Data Science Internship at Direct Assurance    \n"
-        "-French insurance company, part of Axa Group         \n"
-        "In charge of statistical studies in the Data department, I am monitoring the KPI's evolution \
-        and presenting weekly reports to the board. I am crunching data to answer board of \
-        Directors business requests, and then automatig these data analysis with python, SQL and HTML dashboards.\
+        "_-French insurance company, part of Axa Group_         \n"
+        "In charge of statistical studies in the Data department,  \n I am monitoring the KPI's evolution \
+        and presenting weekly reports to the board.   \n I am crunching data to answer board of \
+        Directors business requests, and then automatig these data analysis with python, SQL and HTML dashboards.    \n \
         I help predictiong competitors advertising investment by building \
-        a machine-learning algorithm (a Gradient Boosted Regression Trees) based on the company data.\
-        I am also in charge of exploring new data bought to a survey company (Yougov).\
+        a machine-learning algorithm (a Gradient Boosted Regression Trees) based on the company data.    \n \
+        I am also in charge of exploring new data bought to a survey company (Yougov).   \n \
         I made hypothesis and created new KPI to straighten the global understanding of the company business, \
         with a focus on the Covid period.")
     
     experiences.markdown(
-        "__2020__: [DataChallenge](https://datachallenge.sfrnet.org/) for the French Radiology Society Congress. Subject: \
+        "__2020__: [DataChallenge] for the French Radiology Society Congress.    \n \
+        _https://datachallenge.sfrnet.org/_    \n \
+        Subject:   \n \
         Automatic assessment of severity of coronary artery disease through AI assisted \
         coronary artery calcium score computation.    \n"
         "Main concepts implemented:    \n"
@@ -25,40 +27,40 @@ def experiences():
         "   - DeepLab  v3+ 3D from the paper 'Encoder-Decoder with Atrous Separable Convolution for Semantic Image Segmentation'.")
     
     experiences.markdown(
-        "__2019__ : Machine learning company project \n"
-        "-ZF Autocruise, part of ZF Friedrichshafen an outfitter car company \n"
-        "Classification method applied to error process (Data processing, machine-learning with Python). \
-        Managing a team of 5 students coming from different countries (China, Tunisia and Brazil) to classify error from a chain of production. \
-        Automatization of hand process in order to help the company to operate its digital transformation. \
+        "__2019__ : Machine learning company project   \n"
+        "_-ZF Autocruise, part of ZF Friedrichshafen an outfitter car company_   \n"
+        "Classification method applied to error process (Data processing, machine-learning with Python).   \n \
+        Managing a team of 5 students coming from different countries (China, Tunisia and Brazil) to classify error from a chain of production.   \n \
+        Automatization of hand process in order to help the company to operate its digital transformation.   \n \
         The result was a program that with the data of the defective part (radar parts) sort the error in one of the 37 different default type.")
     
     experiences.markdown(
-        "__2019__: Internship at Zodiac Aerospace \n"
-        "-French aerospace company, part of Safran Group \n"
-        "Managed and optimized the packing and dispatching processing of spare parts returns.\
+        "__2019__: Internship at Zodiac Aerospace   \n"
+        "_-French aerospace company, part of Safran Group_   \n"
+        "Managed and optimized the packing and dispatching processing of spare parts returns.   \n \
         New processes implemented did result in a increase of 10% in productivity (nb of parts dispatched / day) \
         and a decrease of 3% in dispatching errors.")
     
     experiences.markdown(
-        "__2019__: Chairman at Student Union official, \n"
-        "IMT Atlantique  \n"
-        "Managed the merger of a variety of groups within the school (change & people management), \
-        defined and wrote the new association status with lawyer.\
-        Managed and controlled the association budget (150 000€ / year).\
+        "__2019__: Chairman at Student Union official,   \n"
+        "_IMT Atlantique_    \n"
+        "Managed the merger of a variety of groups within the school (change & people management).    \n \
+        Defined and wrote the new association status with lawyer.   \n \
+        Managed and controlled the association budget (150 000€ / year).   \n \
         Event manager (integration’s weekend, regional sport competition, ...).")
 
     experiences.markdown(
-        "__2020__: Student Bar Renovation, \n"
-        "IMT Atlantique \n"
+        "__2020__: Student Bar Renovation,   \n"
+        "_IMT Atlantique_   \n"
         "Built and managed the whole project, from inception to achievement, \
-        including finding funding (50 000 €), sponsors (AB Inbev) and negotiation with all stakeholders.\
+        including finding funding (50 000 €), sponsors (AB Inbev) and negotiation with all stakeholders.   \n\
         Project successfully achieved in 10 months and on budget.")
     
     experiences.markdown(
-        "__2016__: Internship at La Banque Postale \n"
-        "Bank arm of the French Postal Group \n"
-        "Managing an Incentive on Northeast post stations to straighten the economic implantation of the company.\
-        Rewarding post offices that get the best progression during the incentive. \
+        "__2016__: Internship at La Banque Postale   \n"
+        "_Bank arm of the French Postal Group_   \n"
+        "Managing an Incentive on Northeast post stations to straighten the economic implantation of the company.  \n\
+        Rewarding post offices that get the best progression during the incentive.   \n\
         Built an excel-based analysis and Data Mining tool in order to define what’s progression regardless of sales revenue.")
         
 def education():
@@ -82,11 +84,11 @@ def skills():
     skills = st.beta_expander("Skills")
     skills.markdown(
         "__IT – Programming__ : \n"
-        "Python: I've an advanced knowledge of Python, including object oriented programming\n"
-        "R, SQL, (proficient) \
-        Java, Julia (informal) \
-        Proficient in MS Office tools \
-        Basic Front-end knowledge (JavaScript, Visual Studio) \
+        "Python: I have an advanced knowledge of Python, including object oriented programming  \n"
+        "R, SQL, (proficient)   \n\
+        Java, Julia (informal)   \n\
+        Proficient in MS Office tools   \n\
+        Basic Front-end knowledge (JavaScript, HTML) \
             ")
     skills.markdown(
         "__Machine-learning__ : \n"
