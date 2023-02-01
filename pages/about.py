6484e4ab5901239e767c6c9536645bd142f37f25
Luther <56174379+Sma6500@users.noreply.github.com>
2023-02-01 16:44:32 +0100

    Update about.py

--- pages/about.py
+++ pages/about.py
@@ -3,11 +3,10 @@ import streamlit as st
 def content():
     st.title('About me')
     st.write(
-        "I'm a 23 french engineering student at __IMT Atlantique__, one of the top french _grandes écoles_.    \n"
-        "Currently in my last year, I study medical image processing and make deep-learning project \
-        until February. \n"
-        "I'm looking for an end-of-study internship starting end of March."
-        "During my studies, I specialised in __Deep-learning__, __Data analysis__ and its  \
+        "I'm a 24 french research engineer at __LOCEAN__ (Laboratoire d'Océanographie et du Climat : Experimentation et Approches Numériques) \n"
+        "Currently studying Phytoplankton community change under Climate Change Scenarios using Neural Network methods \n"
+        "During my studies at __IMT Atlantique__ (one of the top french grandes écoles), \n" 
+        "I specialised in __Deep-learning__, __Data analysis__ and its \
         applications in Health, renewable energy and digital business.\n"
         "I especially love implementing __deep-learning__ algorithms and I'm very interested in  \
         improving and putting in practice my skills in this field.\n"
